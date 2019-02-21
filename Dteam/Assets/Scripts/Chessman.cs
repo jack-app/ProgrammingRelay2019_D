8f8cd514d2ce8101596ccb50937619bba994459f
LAPTOP-KAR0J5S8\suginomasashi <sgnmssuioaai@gmail.com>
2019-02-21 12:03:55 +0900

    commit

---
+++ Dteam/Assets/Scripts/Chessman.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chessman : MonoBehaviour
+{
+
+    public Vector3 position;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Move(Vector3 moveposition)
+    {
+        transform.position = moveposition;
+        position = moveposition;
+    }
+}
